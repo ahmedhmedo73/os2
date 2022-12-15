241f43b448fa21788221a1666355bf60ae02f0c7
ahmed kamel <58473744+ahmedhmedo73@users.noreply.github.com>
2022-12-15 23:03:55 +0200

    ..

--- Bounded_Buffer_Problem-master/src/Producer.java
+++ Bounded_Buffer_Problem-master/src/Producer.java
@@ -1,40 +1,21 @@
-import java.util.Queue;
-import java.util.Random;
-import java.util.concurrent.Semaphore;
-
 public class Producer extends Thread {
+	private final Buffer buffer;
+	private final int id;
 
-	private Semaphore fillcount;
-	private Semaphore emptycount;
-	private Random randGen;
-	private Queue<Integer> queue;
-	
-	public Producer(Semaphore fillcount, Semaphore emptycount, Queue<Integer> queue) {
-		this.fillcount = fillcount;
-		this.emptycount = emptycount;
-		randGen = new Random(1);
-		this.queue = queue;
+	public Producer(Buffer buffer, int id) {
+		this.buffer = buffer;
+		this.id = id;
 	}
-	
+
 	@Override
 	public void run() {
-		while(true) {
+		while (true) {
 			try {
-				if (emptycount.availablePermits()>0) {
-					int n = Math.abs((randGen.nextInt(10)) % 1000);
-					queue.add(n);
-					fillcount.release();
-					emptycount.acquire();
-					System.out.println("Producer produce " + n);
-				} else {
-					System.out.println("The queue is full producer can't produce");
-				}
-				sleep(Math.abs(randGen.nextInt()) % 1000 + 1000);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
+				this.buffer.insert(System.currentTimeMillis(), this.id);
+				sleep((long) (Math.random() * 1000));
+			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
 }
--- Bounded_Buffer_Problem-master/src/Program.java
+++ Bounded_Buffer_Problem-master/src/Program.java
@@ -1,28 +1,22 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.concurrent.Semaphore;
 
 public class Program {
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Started");
+		Buffer buffer = new Buffer(3);
 
-	public static final int BUFFER_SIZE = 3;
-	private Semaphore fillcount;
-	private Semaphore emptycount;
-	private Queue<Integer> queue;
+		Consumer c1 = new Consumer(buffer, 1);
+		Consumer c2 = new Consumer(buffer, 2);
+		Producer p1 = new Producer(buffer, 1);
+		Producer p2 = new Producer(buffer, 2);
 
-	public Program() {
-		fillcount = new Semaphore(0);
-		emptycount = new Semaphore(BUFFER_SIZE);
-		queue = new LinkedList<>();
-		Producer prod = new Producer(fillcount, emptycount, queue);
-		Consumer cons = new Consumer(fillcount, emptycount, queue);
-		prod.start();
-		cons.start();
+		c1.start();
+		c2.start();
+		p1.start();
+		p2.start();
+
+		c1.join();
+		c2.join();
+		p1.join();
+		p2.join();
 	}
-	
-	
-	public static void main(String[] args) {
-		@SuppressWarnings("unused")
-		Program prog = new Program();
-	}
-	
 }
